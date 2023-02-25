a96bc768325ce0c2f0f565015c8b5c3613c1b16c
Talves <126295566+1050482023011@users.noreply.github.com>
2023-02-24 21:51:54 -0300

    Atividade

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,14 @@
-alert("Ola")
+//alert("Ola")
+
+//const a = 10
+//const b =20
+
+const a = Number(prompt("Digite o valor do seu salario: "))
+const B = Number(prompt("Digite a porcentagem do aumento: "))
+
+
+resultado = (a*(b/100)+1)
+console.log(resultado)
+
+
+document.write(resultado)
