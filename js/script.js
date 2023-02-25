ba6e45414cc9de19e2e030353c31ba30cd9a0c29
Talves <126295566+1050482023011@users.noreply.github.com>
2023-02-24 22:19:37 -0300

    thiago

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const a = Number(prompt("Digite o valor do seu salario: "))
 const b = Number(prompt("Digite a porcentagem do aumento: "))
 
 
-resultado = (a*(b/100)+1)
+resultado = a*((b/100)+1)
+//resultado = a+(a*b/100)
 console.log(resultado)
 
 
